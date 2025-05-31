580e3f94fedc03a57cfc70734afd78ce8c254e02
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2025-05-31 20:07:33 -0300

    Acho que agora tá funcionando

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -957,7 +957,7 @@ asmlinkage long sys_open_tree_attr(int dfd, const char __user *path,
 				   size_t usize);
 
 // SYSCALLS NOVAS
-asmlinkage long sys_stack_create(int val, void __user *ptr);
+asmlinkage long sys_stack_create(int val, void __user **ptr);
 asmlinkage long sys_stack_destroy(void __user *ptr);
 asmlinkage long sys_stack_append(void __user *ptr, int val);
 asmlinkage long sys_stack_pop(void __user *ptr, int __user *out_val);
--- kernel/stack.c
+++ kernel/stack.c
@@ -9,32 +9,38 @@ struct Stack {
   struct Node* top;
 };
 
-SYSCALL_DEFINE2(stack_create, int, val, void __user *, ptr){
-  struct Stack* stack = (struct Stack*)ptr;
+SYSCALL_DEFINE2(stack_create, int, val, void __user **, ptr){
+  pr_info("### Criando stack");
+  struct Stack* stack = kmalloc(sizeof(struct Stack), GFP_KERNEL);
   struct Node* node = kmalloc(sizeof(struct Node), GFP_KERNEL);
   node->prev = NULL;
   node->val = val;
   stack->top = node;
+  if(copy_to_user(ptr, &stack, sizeof(void*))){
+	  return -EFAULT;
+  }
   return 0;
 }
 
 SYSCALL_DEFINE1(stack_destroy, void __user *, ptr){
+  pr_info("### Começando a destruir o Stack");
   struct Stack* stack = (struct Stack*)ptr;
   if (stack == NULL) {
-	  return -1;
+	  return -EFAULT;
   }
   struct Node *node = stack->top;
   while (node != NULL) {
     struct Node *next = node->prev;
-    pr_info("### Liberando um nó de valor %d", node->val);
+    pr_info("### Destroy: Liberando um nó de valor %d", node->val);
     kfree(node);
     node = next;
   }
-  // Não cabe ao kernel liberar o espaço alocado pelo stack. Cabe ao usuário
+  kfree(stack);
   return 0;
 }
 
 SYSCALL_DEFINE2(stack_append, void __user *, ptr, int, val){
+  pr_info("### Adicionando valor %d ao stack", val);
   struct Stack *stack = (struct Stack*)ptr;
   struct Node *new = kmalloc(sizeof(int), GFP_KERNEL);
   new->prev = stack->top;
@@ -50,6 +56,10 @@ SYSCALL_DEFINE2(stack_pop, void __user *, ptr, int __user *, out_val){
   }
   struct Node* curr = stack->top;
   *out_val = curr->val;
+  if(copy_to_user(out_val, &curr->val, sizeof(int))){
+	  return -EFAULT;
+  }
+  pr_info("### Pop do valor %d", curr->val);
   stack->top = stack->top->prev;
   kfree(curr);
   return 0;
